8f1207ea082c86a0afb46e4c257e24d5628adbd8
Michael <michael@tricht.dev>
2020-01-10 00:37:39 +0100

    Generate autoupdater.xml

---
+++ src/main/java/dev/tricht/lunaris/AutoUpdateConfiguration.java
@@ -0,0 +1,43 @@
+package dev.tricht.lunaris;
+
+import lombok.extern.slf4j.Slf4j;
+import org.update4j.Configuration;
+import org.update4j.FileMetadata;
+import org.update4j.OS;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class AutoUpdateConfiguration {
+    public static void main(String[] args) throws IOException {
+        File target = new File("target");
+        Optional<FileMetadata.Reference> shadedJar = FileMetadata.streamDirectory(target.getAbsolutePath())
+                .filter(r -> !r.getSource().toString().endsWith("-shaded.jar"))
+                .findFirst();
+        if (!shadedJar.isPresent()) {
+            log.error("Shaded jar not found.");
+            System.exit(1);
+            return;
+        }
+        FileMetadata.Reference shadedJarFile = shadedJar.get()
+                .uri("https://github.com/mtricht/lunaris/releases/download/v0.3.1/lunaris-0.3.1.jar")
+                .classpath(true)
+                .path("lunaris.jar")
+                .os(OS.WINDOWS)
+                .ignoreBootConflict(true);
+        Configuration build = Configuration.builder()
+                .basePath("${user.dir}/app")
+                .property("default.launcher.main.class", "dev.tricht.lunaris.Lunaris")
+                .files(List.of(shadedJarFile))
+                .build();
+
+        FileWriter writer = new FileWriter("autoupdater.xml");
+        build.write(writer);
+        writer.flush();
+        writer.close();
+    }
+}
