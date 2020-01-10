35514ecc10959806f3903b933d8e220c25d39ff7
Michael <michael@tricht.dev>
2020-01-10 01:55:51 +0100

    Fix lunaris.bat when running as administrator.

--- scripts/lunaris.bat
+++ scripts/lunaris.bat
@@ -1,4 +1,5 @@
 @echo off
+cd %~dp0
 start image\bin\javaw.exe -jar update4j.jar --remote https://raw.githubusercontent.com/mtricht/lunaris/master/autoupdater.xml --singleInstance
 echo Starting lunaris, checking for updates...
 timeout /t 3 > NUL
