cb2a7748aa9a98e8791a34d7ee5f0fb772af71e6
Michael <michael@tricht.dev>
2020-05-10 12:57:55 +0200

    Remove unused and commented code.

--- src/main/java/dev/tricht/lunaris/info/poeprices/PoePricesAPI.kt
+++ src/main/java/dev/tricht/lunaris/info/poeprices/PoePricesAPI.kt
@@ -1,38 +1,24 @@
 package dev.tricht.lunaris.info.poeprices
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import dev.tricht.lunaris.Lunaris
 import dev.tricht.lunaris.item.Item
 import dev.tricht.lunaris.util.Properties
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-import java.io.IOException
 import java.util.Base64
 
 class PoePricesAPI() {
 
     fun getItem(item: Item, callback: Callback) {
-        if (Properties.getProperty("trade_search.poeprices") == "0") {
-            return
-        }
         val itemEncoded = Base64.getEncoder().encodeToString(item.clipboardText.toByteArray())
         val request = Request.Builder().url(
                 String.format("https://www.poeprices.info/api?l=%s&i=%s", Properties.league, itemEncoded)
         ).build()
-        //try {
-            client.newCall(request).enqueue(callback)
-//            objectMapper.readValue(response.body!!.string(), ItemPricePrediction::class.java)
-//        } catch (e: IOException) {
-//            log.error("Failed to call poeprices.info", e)
-//            null
-//        }
+        client.newCall(request).enqueue(callback)
     }
 
     companion object {
-        private val log = org.slf4j.LoggerFactory.getLogger(Lunaris::class.java)
         private var client = OkHttpClient()
-        private var objectMapper = ObjectMapper()
     }
 }
