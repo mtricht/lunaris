c59bdf3955fe71d66f6e48d3b119322a32f6eef4
Eduardo Bueno <eduardo.bueno@ifood.com.br>
2020-11-09 19:47:47 -0300

    Better variable names

--- src/main/java/dev/tricht/lunaris/item/LocalAffixResolver.java
+++ src/main/java/dev/tricht/lunaris/item/LocalAffixResolver.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 
 public class LocalAffixResolver {
     private final static String LOCAL = " (Local)";
-    final static List<Pattern> localArmorTypes = List.of(
+    final static List<Pattern> localArmorAffixes = List.of(
             Pattern.compile(".* to Armour"),
             Pattern.compile(".* to Evasion Rating"),
             Pattern.compile(".* to maximum Energy Shield"),
@@ -20,7 +20,7 @@ public class LocalAffixResolver {
             Pattern.compile(".* increased Armour and Energy Shield"),
             Pattern.compile(".* increased Evasion and Energy Shield"),
             Pattern.compile(".* increased increased Armour, Evasion and Energy Shield"));
-    final static List<Pattern> localWeaponTypes = List.of(
+    final static List<Pattern> localWeaponAffixes = List.of(
             Pattern.compile("Adds .* to .* Physical Damage"),
             Pattern.compile("Adds .* to .* Chaos Damage"),
             Pattern.compile("Adds .* to .* Cold Damage"),
@@ -32,9 +32,9 @@ public class LocalAffixResolver {
 
     public static String resolve(String affix, ItemType itemType) {
         if ((itemType instanceof EquipmentItem) || (itemType instanceof WeaponItem)) {
-            if (itemType instanceof EquipmentItem && localArmorTypes.stream().anyMatch(p -> p.matcher(affix).matches())) {
+            if (itemType instanceof EquipmentItem && localArmorAffixes.stream().anyMatch(p -> p.matcher(affix).matches())) {
                 return affix + LOCAL;
-            } else if (itemType instanceof  WeaponItem && localWeaponTypes.stream().anyMatch(p -> p.matcher(affix).matches())) {
+            } else if (itemType instanceof  WeaponItem && localWeaponAffixes.stream().anyMatch(p -> p.matcher(affix).matches())) {
                 return affix + LOCAL;
             }
         }
